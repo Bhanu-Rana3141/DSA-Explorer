8c247ade3a6f2267706fa6649562fa2d9cea1dae
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2024-02-03 23:14:20 +0530

    Add files via upload

--- 05 Linkedlist/13_removeNthNodeFromLast.cpp
+++ 05 Linkedlist/13_removeNthNodeFromLast.cpp
@@ -1,3 +1,15 @@
+/*
+
+- APPROACH 
+
+* Reverse List
+* Remove Nth node
+* Reverse again
+* TC - O(N)
+* SC - O(1)
+
+*/
+
 #include<iostream>
 using namespace std;
 
@@ -32,6 +44,47 @@ void print(Node* head) {
     cout << endl;
 }
 
+Node* reverse(Node* head) {
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* forward = NULL;
+
+    while(curr != NULL) {
+        forward = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = forward;
+    }
+    return prev;
+}
+
+Node* removeNode(Node* head, int n) {
+
+    if(head == NULL || head -> next == NULL) {
+        return NULL;
+    }
+
+    Node* temp = reverse(head);
+    Node* t2 = temp;
+
+    if(n == 1) {
+        temp = temp -> next;
+        head = reverse(temp);
+        return head;
+    }
+        
+    int i = 1;
+    while(i < n-1) {
+        t2 = t2 -> next;
+        i++;
+    }
+
+    t2 -> next = t2 -> next -> next;
+    head = reverse(temp);
+
+    return head;
+}
+
 int main() {
 
     int n;
@@ -50,7 +103,12 @@ int main() {
     cout << "Linkedlist: ";
     print(head);
 
-    
-    
+    int k;
+    cout << "Enter k: ";
+    cin >> k;
+
+    Node* ans = removeNode(head, k);
+    print(ans);
+
     return 0;
 }
--- 05 Linkedlist/15_rotateList.cpp
+++ 05 Linkedlist/15_rotateList.cpp
@@ -8,6 +8,101 @@ APPROACH 2 - USE CONCEPT OF ARRAYS
 * SC - O(1)
 
 APPROACH 3 - TRAVERSAL
-
+* Find length - O(N)
+* Traverse and reach at new head node len-k , O(N) - in worst case if k = len
+* TC - O(N)
+* SC - O(1)
 
 */
+
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtTail(Node* &head, Node* &tail, int data) {
+    Node* newNode = new Node(data);
+    if(head == NULL) {
+        head = newNode;
+        tail = newNode;
+    }
+    else {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+void print(Node* head) {
+    while(head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+Node* rotateList(Node* head, int k) {
+
+    if(head == NULL || head->next == NULL) {
+        return head;
+    }
+    if(k == 0) {
+        return head;
+    }
+
+    Node* temp = head;
+    int len = 1;
+
+    while(temp->next != NULL) {
+        len++;
+        temp = temp->next;
+    }    
+
+    temp->next = head;
+
+    temp = head;
+    for(int i=1; i<len-k; i++) {
+        temp = temp->next;
+    }
+
+    head = temp->next;
+    temp->next = NULL;
+
+    return head;
+}
+
+int main() {
+
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    for(int i=0; i<n; i++) {
+        int input;
+        cin >> input;
+        insertAtTail(head, tail, input);
+    }
+
+    cout << "Linkedlist: ";
+    print(head);
+
+    int k;
+    cout << "Enter k: ";
+    cin >> k;
+
+    Node* ans = rotateList(head, k);
+    print(ans);
+
+    return 0;
+}
